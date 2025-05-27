b74f9b56ee8d42b6d5ab5cc9b440d881885066c1
Adrien Blanchet <adrien.blanchet@live.fr>
2025-05-27 19:53:04 +0200

    build file for any perl version

--- build.bat
+++ build.bat
@@ -5,21 +5,21 @@
 set HERE=%~dp0
 
 :: Does perl is installed here ?
-if not exist "%HERE%\strawberry-perl-5.16.3.1-32bit-portable\portableshell.bat" (
-    echo Expected %HERE%\strawberry-perl-5.16.3.1-32bit-portable\portableshell.bat has not been found.
+if not exist "%HERE%\strawberry-perl-*32*\portableshell.bat" (
+    echo Expected %HERE%\strawberry-perl-*32*\portableshell.bat has not been found.
     echo Please download the corresponding archive on http://strawberryperl.com/releases.html
     echo Will try to compile with system wide perl.
 )
 
 :: Adding local perl libs
 setlocal
-set PATH=%HERE%\strawberry-perl-5.16.3.1-32bit-portable\perl\bin;%PATH%
-set PATH=%HERE%\strawberry-perl-5.16.3.1-32bit-portable\perl\site\bin;%PATH%
+set PATH=%HERE%\strawberry-perl-*32*\perl\bin;%PATH%
+set PATH=%HERE%\strawberry-perl-*32*\perl\site\bin;%PATH%
 
-@REM if not defined %PATH% or echo ;%PATH%; | find /C /I ";<%HERE%\strawberry-perl-5.16.3.1-32bit-portable\perl\bin>;"(
+@REM if not defined %PATH% or echo ;%PATH%; | find /C /I ";<%HERE%\strawberry-perl-*32*\perl\bin>;"(
 @REM
 @REM )
-@REM if echo ;%PATH%; | find /C /I ";<%HERE%\strawberry-perl-5.16.3.1-32bit-portable\perl\site\bin>;"(
+@REM if echo ;%PATH%; | find /C /I ";<%HERE%\strawberry-perl-*32*\perl\site\bin>;"(
 @REM
 @REM )
 
