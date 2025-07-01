6462255fe36803e3a1b415bb7596fb4bd3cc01dd
Adrien Blanchet <adrien.blanchet@live.fr>
2025-07-01 23:19:28 +0200

    restoring delete

--- kse.pl
+++ kse.pl
@@ -4593,7 +4593,8 @@ sub SpawnOtherAreasWidgets{
         -command=>sub {
             CommitChanges($treeitem);
 
-            Populate_OtherAreas($treeitem);
+            # how do we handle more than one?
+            $tree->delete('entry', $treeitem);
 
             #unspawn widgets
             for my $widge (@spawned_widgets, $picture_label) {
